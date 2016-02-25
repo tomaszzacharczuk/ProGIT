3e33f466855c9cada2bbf962b48d4f5744606c78
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2016-02-25 11:49:13 +0100

    Added Pet table listing to sqlite

--- sqlite3_example.py
+++ sqlite3_example.py
@@ -4,6 +4,12 @@ conn.execute("CREATE TABLE IF NOT EXISTS pets (type, breed, gender, name)")
 conn.execute("INSERT INTO pets VALUES ('dog', 'begel', 'male', 'Oscar')")
 conn.execute("INSERT INTO pets VALUES ('cat', 'british', 'female', 'Kate')")
 results = conn.execute("SELECT * FROM pets")
+print('Pets table:')
+for result in results:
+	print(result)
+print('Pet table:')
+conn = sqlite3.connect('pet.db')
+results = conn.execute("SELECT * FROM pet")
 for result in results:
 	print(result)
 conn.close()
