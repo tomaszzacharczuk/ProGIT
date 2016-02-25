c0f5dc0ee7f623c162250df962e732e7e5f8b4c4
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2016-02-25 12:47:16 +0100

    added new cat to DB

--- sqlite3_example.py
+++ sqlite3_example.py
@@ -3,6 +3,7 @@ conn = sqlite3.connect('pets.db')
 conn.execute("CREATE TABLE IF NOT EXISTS pets (type, breed, gender, name)")
 conn.execute("INSERT INTO pets VALUES ('dog', 'begel', 'male', 'Oscar')")
 conn.execute("INSERT INTO pets VALUES ('cat', 'british', 'female', 'Kate')")
+conn.execute("INSERT INTO pets VALUES ('cat', 'siberian', 'female', 'Susanna')")
 results = conn.execute("SELECT * FROM pets")
 print('Pets table:')
 for result in results:
